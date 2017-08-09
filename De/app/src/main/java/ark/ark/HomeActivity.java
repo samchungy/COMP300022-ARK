f192fc9b5e3071769967beb9ee1cdf02664c5150
Zengster <hanruz@student.unimelb.edu.au>
2017-08-09 11:54:13 +1000

    it begins
    bringing over stuff from the other repository

---
+++ De/app/src/main/java/ark/ark/HomeActivity.java
@@ -0,0 +1,101 @@
+package ark.ark;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import layout.ChatFragment;
+import layout.HomeFragment;
+import layout.MapFragment;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private Fragment frag_home = new HomeFragment();
+    private Fragment frag_chat = new ChatFragment();
+    private Fragment frag_map = new MapFragment();
+    private Fragment frag_profile = new ProfileFragment();
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    switchTo("Home");
+                    return true;
+                case R.id.navigation_chat:
+                    switchTo("Chat");
+                    return true;
+                case R.id.navigation_map:
+                    switchTo("Map");
+                    return true;
+                case R.id.navigation_profile:
+                    switchTo("Profile");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        ButterKnife.bind(this);
+        //TODO
+
+        // switch to home page after entering the app
+        switchTo("Home");
+    }
+
+
+
+
+
+    // switching to different pages
+    private void switchTo(String page) {
+        Fragment targetFrag;
+
+        switch (page) {
+            case "Home":
+                targetFrag = frag_home;
+                break;
+            case "Chat":
+                targetFrag = frag_chat;
+                break;
+            case "Map":
+                targetFrag = frag_map;
+                break;
+            case "Profile":
+                targetFrag = frag_profile;
+                break;
+            default:
+                targetFrag = frag_home;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, targetFrag).commit();
+
+    }
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# ARK
+# IT-Project
+// Made by group ARK
+
+Dev Intro:
+
+I have made a preliminary skeleton for the app, and now the client-side contains the followings:
+
+-   a main activity that contains a bottom navigation bar and 4 fragments
+-   4 fragments: Home, Chat, Map, Profile, their names are self-explanatory
+
+Functionalities needed for each of the 4 fragments:
+    Home: do welcoming sort of bullshit
+    Chat: can connect to our server(which I will be implementing) and load/send chat messages
+    Map: features we discussed last week
+    Profile: user info, settings
+
+
+I recommend each of us clone the client-side from git and give it a run.
