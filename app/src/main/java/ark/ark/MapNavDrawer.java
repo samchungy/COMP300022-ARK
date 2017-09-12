65384cf5df21833d73b15150429dd792a44a4078
samchungy <samchungy@gmail.com>
2017-09-12 21:28:48 +1000

    Show Other User in Waypoint Implemented

--- app/src/main/java/ark/ark/MapNavDrawer.java
+++ app/src/main/java/ark/ark/MapNavDrawer.java
@@ -22,6 +22,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -41,6 +48,13 @@ import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ark.ark.Authentication.ARK_auth;
+
+import static ark.ark.Authentication.ARK_auth.fetchUserEmail;
+
 public class MapNavDrawer extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         OnMapReadyCallback,
@@ -54,6 +68,7 @@ public class MapNavDrawer extends AppCompatActivity
     private UiSettings uiSettings;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     private Marker mWaypoint;
+    private Marker mPerson;
     private BottomSheetBehavior mBottomSheetBehavior;
     protected GeoDataClient mGeoDataClient;
     protected PlaceDetectionClient mPlaceDetectionClient;
@@ -99,6 +114,39 @@ public class MapNavDrawer extends AppCompatActivity
         mGeoDataClient = Places.getGeoDataClient(this, null);
         // Construct a PlaceDetectionClient.
         mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        //Initiate RequestQueue
+        // Instantiate the RequestQueue.
+        final TextView mTextView = (TextView) findViewById(R.id.textView2);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String useremail = ARK_auth.fetchUserEmail(this);
+        mTextView.setText(useremail);
+        String url ="http://52.65.97.117/locations/show?email=" + useremail;
+
+        // Request a json response from the provided URL.
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject loc = response.getJSONObject("location");
+                            showPerson(new LatLng(loc.getDouble("lat"),loc.getDouble("lng")));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
 
     }
 
@@ -255,5 +303,16 @@ public class MapNavDrawer extends AppCompatActivity
         mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
 
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }
+
+    public void showPerson(LatLng lat){
+        mPerson = mMap.addMarker(new MarkerOptions()
+                .position(lat)
+                .title("Other Person"));
+    }
+
+    public LatLng getCoords(JSONObject response) throws JSONException {
+        LatLng coord = new LatLng(response.getDouble("lat"), response.getDouble("lng"));
+        return coord;
     }
 }
