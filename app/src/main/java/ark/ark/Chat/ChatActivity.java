d4ec8ad92b76ea681a30f3fd36b5ba6eafe0d347
hungrydaddy <masonaustin1996@yahoo.com>
2017-10-16 19:56:37 +1100

    added chat activity.

---
+++ app/src/main/java/ark/ark/Chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package ark.ark.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ark.ark.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
